5b52f79505730ff2243b8e09998e95b956d1d6ff
notCan <devecann@gmail.com>
2022-05-15 23:45:20 +0300

    added home second section

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <section id="hero" class="pb-32">
-      <div class="lanista-container__index content h-auto">
+      <div class="lanista-container__small content h-auto">
         <div class="py-16">
           <lui-chip
             rounded
@@ -20,6 +20,170 @@
         </div>
       </div>
     </section>
+    <section id="developmentProcess" class="bg-section">
+      <div class="design lanista-container__small min-h-[534px] pt-24">
+        <div class="flex flex-row justify-between">
+          <div class="flex flex-col">
+            <div>
+              <lui-chip
+                rounded
+                size="lg"
+                class="bg-chip text-secondary justify-center items-center max-w-12"
+              >
+                <lui-icon name="shape-2" line size="lg" class="mt-3" />
+              </lui-chip>
+            </div>
+            <h1 class="text-white font-semibold text-4xl pt-8">Ui/Ux design</h1>
+          </div>
+          <div class="w-2/3 flex flex-col">
+            <p class="text-heading pb-8 pt-16">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id
+              vulputate magna vel libero accumsan nisl. Sociis fringilla tempor
+              ut et mattis auctor condimentum sit at.
+            </p>
+            <div>
+              <lui-button
+                rounded
+                size="lg"
+                variant="secondary"
+                class="hidden lg:flex"
+                type="outline"
+                icon="arrow-right-up"
+                >Start a project</lui-button
+              >
+            </div>
+            <div class="pt-16 pb-24">
+              <lui-chip rounded class="bg-chip">
+                <nuxt-img src="/images/design.png" class="p-8"></nuxt-img>
+              </lui-chip>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="frontend lanista-container__small min-h-[534px] pt-24">
+        <div class="flex flex-row justify-between">
+          <div class="flex flex-col">
+            <div>
+              <lui-chip
+                rounded
+                size="lg"
+                class="bg-chip text-secondary justify-center items-center max-w-12"
+              >
+                <lui-icon name="shape-2" line size="lg" class="mt-3" />
+              </lui-chip>
+            </div>
+            <h1 class="text-white font-semibold text-4xl pt-8">
+              Frontend Development
+            </h1>
+          </div>
+          <div class="w-2/3 flex flex-col">
+            <p class="text-heading pb-8 pt-16">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id
+              vulputate magna vel libero accumsan nisl. Sociis fringilla tempor
+              ut et mattis auctor condimentum sit at.
+            </p>
+            <div>
+              <lui-button
+                rounded
+                size="lg"
+                variant="secondary"
+                class="hidden lg:flex"
+                type="outline"
+                icon="arrow-right-up"
+                >Start a project</lui-button
+              >
+            </div>
+            <div class="pt-16 pb-24">
+              <lui-chip rounded class="bg-chip">
+                <nuxt-img src="/images/frontend.png" class="p-8"></nuxt-img>
+              </lui-chip>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="backend lanista-container__small min-h-[534px] pt-24">
+        <div class="flex flex-row justify-between">
+          <div class="flex flex-col">
+            <div>
+              <lui-chip
+                rounded
+                size="lg"
+                class="bg-chip text-secondary justify-center items-center max-w-12"
+              >
+                <lui-icon name="shape-2" line size="lg" class="mt-3" />
+              </lui-chip>
+            </div>
+            <h1 class="text-white font-semibold text-4xl pt-8">
+              Backend Development
+            </h1>
+          </div>
+          <div class="w-2/3 flex flex-col">
+            <p class="text-heading pb-8 pt-16">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id
+              vulputate magna vel libero accumsan nisl. Sociis fringilla tempor
+              ut et mattis auctor condimentum sit at.
+            </p>
+            <div>
+              <lui-button
+                rounded
+                size="lg"
+                variant="secondary"
+                class="hidden lg:flex"
+                type="outline"
+                icon="arrow-right-up"
+                >Start a project</lui-button
+              >
+            </div>
+            <div class="pt-16 pb-24">
+              <lui-chip rounded class="bg-chip">
+                <nuxt-img src="/images/backend.png" class="p-8"></nuxt-img>
+              </lui-chip>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="consult lanista-container__small min-h-[534px] pt-24">
+        <div class="flex flex-row justify-between">
+          <div class="flex flex-col">
+            <div>
+              <lui-chip
+                rounded
+                size="lg"
+                class="bg-chip text-secondary justify-center items-center max-w-12"
+              >
+                <lui-icon name="shape-2" line size="lg" class="mt-3" />
+              </lui-chip>
+            </div>
+            <h1 class="text-white font-semibold text-4xl pt-8">
+              Project Consult
+            </h1>
+          </div>
+          <div class="w-2/3 flex flex-col">
+            <p class="text-heading pb-8 pt-16">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id
+              vulputate magna vel libero accumsan nisl. Sociis fringilla tempor
+              ut et mattis auctor condimentum sit at.
+            </p>
+            <div>
+              <lui-button
+                rounded
+                size="lg"
+                variant="secondary"
+                class="hidden lg:flex"
+                type="outline"
+                icon="arrow-right-up"
+                >Start a project</lui-button
+              >
+            </div>
+            <div class="pt-16 pb-24">
+              <lui-chip rounded class="bg-chip">
+                <nuxt-img src="/images/consult.png" class="p-8"></nuxt-img>
+              </lui-chip>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -29,8 +193,11 @@ export default {
 }
 </script>
 <style scoped>
-.lanista-container__index {
+.lanista-container__small {
   @apply container mx-auto px-8;
+}
+.lanista-container__large {
+  @apply container mx-auto;
 }
 .content {
   background-image: url('/images/hero.png');
