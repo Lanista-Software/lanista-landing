6a73abdf713f9cbb30a6abadaddf65ebfb940703
ABB65 <bayhanbayramoglu@gmail.com>
2022-05-30 16:51:05 +0300

    firebase config

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   css: ['~/assets/tailwind.css', 'remixicon/fonts/remixicon.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: '~plugins/firebase.js', ssr: false }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
