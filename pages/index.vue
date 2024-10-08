5dc4fcfee2348c2382b863f7ad137bed695d0401
rhmkstk <rahmi.kstk@gmail.com>
2024-10-08 09:30:10 +0300

    fix: hero bg-image

--- pages/index.vue
+++ pages/index.vue
@@ -207,7 +207,7 @@ function handleSectionViewed(id: string) {
 <template>
   <div>
     <!-- Home Section -->
-    <div class="bg-[url('/1727359111545_1624068421380_hero.png')] bg-no-repeat bg-cover bg-fixed bg-center">
+    <div class="bg-[url('/1727359111545_1624068421380_hero.png')] aspect-auto bg-cover bg-center">
       <MolAppSection id="home" @viewed="handleSectionViewed">
         <TemplatesHero />
       </MolAppSection>
